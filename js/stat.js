cf14e8a41d427842b8f93706e3b2384f259a06d5
Volodymyr Padovsky <vld06@ukr.net>
2018-02-10 21:03:24 +0200

    HW2 final

--- js/stat.js
+++ js/stat.js
@@ -1,74 +1,62 @@
 'use strict';
 
-// var getMaxElement = function(arr) {	// max number function
-// 	var max = -1;
-// 	var maxIndex = -1;
+var SCOREBOARD_WIDTH = 420;
+var SCOREBOARD_HEIGHT = 270;
+var SCOREBOARD_X = 100;
+var SCOREBOARD_Y = 10;
 
-// 	for (var i = 0; i < arr.length; i++) {
-// 		var time = arr[i];
-// 		if (time > max) {
-// 			max = time;
-// 			maxIndex = i;
-// 		}
-// 	}
-// 	return max;
-// };
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var GAP = 10;
 
-window.renderStatistics = function(ctx, names, times) {
-	var SCOREBOARD_WIDTH = 420;	//px
-	var SCOREBOARD_HEIGHT = 270; //px
-	var HISTOGRAM_HEIGHT = 150; //px
-	var COLUMN_WIDTH = 40; //px
-	var COLUMN_DISTANCE = 50; //px
-	var COLUMN_COLOR_MY = 'rgba(255, 0, 0, 1)';
-	var COLUMN_COLOR_PLAYERS = 'blue';
-	var opacityRandom = function() {
-		return Math.random().toFixed(1);
-	};
+var BAR_OFFSET = 240;
+var NAME_POSITION = 260;
 
-	var max = -1;
-	var maxIndex = -1;
-
-	for (var i = 0; i < times.length; i++) {
-		var time = times[i];
-		if (time > max) {
-			max = time;
-			maxIndex = i;
-		}
-	}
-
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // black shadow
-	ctx.fillRect(110, 20, 420, 270); // box shadow
-
-	ctx.fillStyle = 'white';  // white
-	ctx.strokeRect(100, 10, 420, 270); // main scoreboard
-	ctx.fillRect(100, 10, 420, 270);
-	
-	ctx.fillStyle = '#000'; 
-	ctx.font = '14px PT Mono';
-	ctx.fillText('Ура вы победили!\nСписок результатов:', 130, 50);
-
-	ctx.fillText('Худшее время:' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
-
-	var step = HISTOGRAM_HEIGHT / (max - 0);
-	var barHeight = 20; //px ширина стовпця
-	var indent = 40; //px відстань між стовпцями
-	var initialX = 80; //px
-	var initialY = 130; //px
-	var lineHeight = 15; //px вистота тексту
-
-	for (var i = 0; i < times.length; i++) {
-	
-		ctx.fillRect(initialY + indent * i, initialX , barHeight, times[i] * step);
-		ctx.fillText(names[i], initialY + lineHeight + indent * i,  initialX + HISTOGRAM_HEIGHT);
-		
-		if (names[i] == 'Вы') {
-			ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-		} else {ctx.fillStyle = '#000';}
-
-	}
-
-	
-	
+var renderScoreboard = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, SCOREBOARD_WIDTH, SCOREBOARD_HEIGHT);
 };
 
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+var getStatus = function (times, players) {
+  var STATUS = false;
+  var SPEED_OF_WINNER = getMaxElement(times);
+  var INDEX_OF_WINNER = times.indexOf(SPEED_OF_WINNER);
+  for (var i = 0; i < times.length; i += 1) {
+    if (players[INDEX_OF_WINNER] === 'Вы') {
+      STATUS = true;
+    }
+  }
+  return STATUS ? 'Ура, вы победили!' : 'Печалька, вы проиграли';
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderScoreboard(ctx, SCOREBOARD_X + GAP, SCOREBOARD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderScoreboard(ctx, SCOREBOARD_X, SCOREBOARD_Y, '#fff');
+  ctx.fillStyle = '#000';
+  ctx.font = ('16px PT Mono');
+  ctx.fillText(getStatus(times, players), SCOREBOARD_X + GAP + GAP, 40);
+  ctx.fillText('Список пользователей:', SCOREBOARD_X + GAP + GAP, 60);
+  for (var i = 0; i < players.length; i += 1) {
+    var MAX_TIME = getMaxElement(times);
+    var CURRENT_BAR_HEIGHT = BAR_HEIGHT / MAX_TIME * times[i];
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'red';
+    } else {
+      ctx.fillStyle = 'blue';
+    }
+    ctx.fillText(Math.floor(times[i]), SCOREBOARD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, BAR_OFFSET - CURRENT_BAR_HEIGHT - GAP);
+    ctx.fillText(players[i], SCOREBOARD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, NAME_POSITION);
+    ctx.fillRect(SCOREBOARD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, BAR_OFFSET - CURRENT_BAR_HEIGHT, BAR_WIDTH, CURRENT_BAR_HEIGHT);
+  }
+};
