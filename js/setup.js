aa4a3868f559adf09d664bf29ca10c149fe38b71
Volodymyr Padovsky <vld06@ukr.net>
2018-02-21 23:59:55 +0200

    HW6 drag and drop added

--- js/setup.js
+++ js/setup.js
@@ -62,4 +62,39 @@
 
   similarListElement.appendChild(fragment);
 
+
+  // Перетаскивание элементов из магазина
+
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function(evt) {
+  	if (evt.target.tagName.toLowerCase() == 'img') {
+  		draggedItem = evt.target;
+  		evt.dataTransfer.setData('text/plain', evt.target.alt)
+  	}
+  });
+
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  artifactsElement.addEventListener('dragover', function(evt) {
+  	evt.preventDefault();
+  	return false;
+  });
+
+  artifactsElement.addEventListener('drop', function(evt) {
+  	evt.target.style.backgroundColor = '';
+  	evt.target.appendChild(draggedItem);
+  	evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function(evt) {
+  	evt.target.style.backgroundColor = 'yellow';
+  	evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function(evt) {
+  	evt.target.style.backgroundColor = '';
+  	evt.preventDefault();
+  });
 })();
